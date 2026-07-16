04f408937949a5229d48ec9175d0ef3533ea97fd
Abdulkabir Musa <musaabdulkabeer19@gmail.com>
2026-07-16 01:15:36 +0100

    chore: use github_private_release strategy for cask downloads

--- homebrew-tap/Casks/vidralo.rb
+++ homebrew-tap/Casks/vidralo.rb
@@ -3,13 +3,13 @@ cask "vidralo" do
 
   on_arm do
     url "https://github.com/AIEraDev/vidralo/releases/download/v#{version}/Vidralo_#{version}_aarch64.dmg",
-        header: "Authorization: Bearer #{ENV["HOMEBREW_GITHUB_API_TOKEN"] || ENV["GITHUB_TOKEN"]}"
+        using: :github_private_release
     sha256 :no_check  # Updated automatically by CI after each release
   end
 
   on_intel do
     url "https://github.com/AIEraDev/vidralo/releases/download/v#{version}/Vidralo_#{version}_x64.dmg",
-        header: "Authorization: Bearer #{ENV["HOMEBREW_GITHUB_API_TOKEN"] || ENV["GITHUB_TOKEN"]}"
+        using: :github_private_release
     sha256 :no_check  # Updated automatically by CI after each release
   end
 
