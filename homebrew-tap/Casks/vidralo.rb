cb75a48ac695ede865e6218001ab1a77e2516cad
Abdulkabir Musa <musaabdulkabeer19@gmail.com>
2026-07-16 01:14:00 +0100

    chore: fix header parameter syntax in Homebrew Cask

--- homebrew-tap/Casks/vidralo.rb
+++ homebrew-tap/Casks/vidralo.rb
@@ -3,17 +3,13 @@ cask "vidralo" do
 
   on_arm do
     url "https://github.com/AIEraDev/vidralo/releases/download/v#{version}/Vidralo_#{version}_aarch64.dmg",
-        headers: {
-          "Authorization" => "Bearer #{ENV["HOMEBREW_GITHUB_API_TOKEN"] || ENV["GITHUB_TOKEN"]}"
-        }
+        header: "Authorization: Bearer #{ENV["HOMEBREW_GITHUB_API_TOKEN"] || ENV["GITHUB_TOKEN"]}"
     sha256 :no_check  # Updated automatically by CI after each release
   end
 
   on_intel do
     url "https://github.com/AIEraDev/vidralo/releases/download/v#{version}/Vidralo_#{version}_x64.dmg",
-        headers: {
-          "Authorization" => "Bearer #{ENV["HOMEBREW_GITHUB_API_TOKEN"] || ENV["GITHUB_TOKEN"]}"
-        }
+        header: "Authorization: Bearer #{ENV["HOMEBREW_GITHUB_API_TOKEN"] || ENV["GITHUB_TOKEN"]}"
     sha256 :no_check  # Updated automatically by CI after each release
   end
 
